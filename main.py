2290249d5c3dcec23d154278933d8d70b74668e9
mucheru-delvan <mkdelvan9@gmail.com>
2026-01-10 23:03:43 +0300

    Removed the display weather function in the main.py file

--- main.py
+++ main.py
@@ -2,33 +2,6 @@ from weather import WeatherFetcher
 from config import DEFAULT_CITY
 
 
-def display_weather(data):
-    location = data["location"]
-    current = data["current"]
-
-    city = location["name"]
-    country = location["country"]
-    local_time = location["localtime"]
-
-    temp = current["temp_c"]
-    feels_like = current["feelslike_c"]
-    condition = current["condition"]["text"]
-    humidity = current["humidity"]
-    wind_speed = current["wind_kph"]
-    wind_dir = current["wind_dir"]
-    is_day = current["is_day"]
-
-    time_icon = "☀️" if is_day == 1 else "🌙"
-
-    print("\n🌤️ Weather Report 🌤️")
-    print(f"City: {city}, {country}")
-    print(f"Local Time: {local_time}\n")
-    print(f"Temperature: {temp}°C (Feels like {feels_like}°C)")
-    print(f"Condition: {condition} {time_icon}")
-    print(f"Humidity: {humidity}%")
-    print(f"Wind: {wind_speed} km/h ({wind_dir})")
-
-
 def write_weather_report_txt(data):
     location = data.get("location", {})
     current = data.get("current", {})
@@ -69,7 +42,6 @@ def main():
         print(f"❌ Error fetching weather data: {data['error']}")
         return
 
-    display_weather(data)
     write_weather_report_txt(data)
 
     print("\n✅ Weather report saved to weather_report.txt")
